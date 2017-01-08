a33b47dd0c40139c2799a7b721407a700ef41575
Zahraa Khalili <zahra2010sw@live.com>
2017-01-08 16:36:17 +0200

    "655"

--- Driver.py
+++ Driver.py
@@ -1,14 +1,11 @@
 import sys
 import random
 import os
-<<<<<<< HEAD
 import numpy as np
 characters={'A':0,'B':1,'C':2,'D':3,'E':4,'F':5,'G':6,'H':7,'I':8,'J':9,'K':10,'L':11,'M':12,'N':13,'O':14,'P':15,'Q':16,'R':17,'S':18,'T':19,'U':20,'V':21,'W':22,'X':23,'Y':24,'Z':25,'a':26,'b':27,'c':28,'d':29,'e':30,'f':31,'g':32,'h':33,'i':34,'j':35,'k':36,'l':37,'m':38,'n':39,'o':40,'p':41,'q':42,'r':43,'s':44,'t':45,'u':46,'v':47,'w':48,'x':49,'y':50,'z':51,' ':52}
 reversedCharacters=characters.__class__(map(reversed, characters.items()))
-=======
 
 # successful input: 2,3 3
->>>>>>> 5316b3f8f3694eadb412665b067b530b082974d9
 
 # reads content from given file
 def readFile(fileName):
@@ -122,12 +119,8 @@ while 1:  # key validity while loop
     # --get key size from user
     print("Please enter the key size.\n"
           "--Note: enter the coordinates in the format of 'N,M'\n"
-<<<<<<< HEAD
           "--Note: both numbers must be greater or equal to 2\n"
           "Key size: ")
-=======
-          "Key size: ", end="")
->>>>>>> 5316b3f8f3694eadb412665b067b530b082974d9
     keySize = sys.stdin.readline()
     keySize = keySize.rstrip('\n')
     keySize = keySize.replace(' ', '')
@@ -200,7 +193,7 @@ while 1:  # block validity while loop
     print("Please enter the desired number of columns in the blocks.\n"
           "--Note: the number of rows will be taken from the key"
           "(the number of columns of the key must equal the number of rows in the block.)\n"
-          "Block size: ", end="")
+          "Block size: ")
     blockSize = sys.stdin.readline()
     blockSize = blockSize.rstrip('\n')
     blockSize = blockSize.replace(' ', '')
